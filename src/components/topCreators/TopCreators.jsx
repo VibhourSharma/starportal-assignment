32b6704dbdc6c5bc002c0551e5652bca2c668c2f
Vibhour Sharma <vibhour121@gmail.com>
2024-07-10 20:24:25 +0530

    🚀fix: creators card play hover effect

--- src/components/topCreators/TopCreators.jsx
+++ src/components/topCreators/TopCreators.jsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import topCryptoCreators from "./data";
 import playSvg from "../../assets/play.svg";
 import reelSvg from "../../assets/reel.svg";
 
 const TopCreators = () => {
+  const [hoveredCard, setHoveredCard] = useState(null);
+  const [showPlayButton, setShowPlayButton] = useState(false);
+  let hoverTimeout;
+
+  const handleMouseEnter = (id) => {
+    hoverTimeout = setTimeout(() => {
+      setHoveredCard(id);
+      setShowPlayButton(true);
+    }, 100);
+  };
+
+  const handleMouseLeave = () => {
+    clearTimeout(hoverTimeout);
+    setShowPlayButton(false);
+  };
+
   return (
     <div className="bg-[#080808]">
       <div className="flex flex-col gap-2 items-center justify-center p-8">
@@ -18,6 +34,8 @@ const TopCreators = () => {
           <div
             key={creator.id}
             className="relative w-64 h-96 overflow-hidden rounded-lg m-3 transform transition duration-300 group"
+            onMouseEnter={() => handleMouseEnter(creator.id)}
+            onMouseLeave={handleMouseLeave}
           >
             <img
               src={creator.imageSrc}
@@ -29,14 +47,16 @@ const TopCreators = () => {
                 {creator.content}
               </p>
             </div>
-            <div className="absolute inset-0 flex items-center justify-center opacity-0 transition-opacity duration-700 hover:opacity-1">
-              <div className="rounded-full flex items-center justify-center w-16 h-16 bg-white/30">
-                <img src={playSvg} alt="Play" className="w-12 h-12" />
-              </div>
-            </div>
+
             <div className="absolute top-4 right-4 rounded-full p-2 bg-white/30 backdrop-blur-md">
               <img src={reelSvg} alt="reel" className="w-6 h-6" />
             </div>
+
+            {hoveredCard === creator.id && showPlayButton && (
+              <div className="absolute inset-0 flex items-center justify-center  duration-300 delay-300 ease-in-out cursor-pointer">
+                <img src={playSvg} alt="play" className="w-12 h-12" />
+              </div>
+            )}
           </div>
         ))}
       </div>
